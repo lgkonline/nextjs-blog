90dbd84efabcd856c21383d5ecf2832cb66df8e4
Lars Kliesing <lgk@live.de>
2020-04-30 17:39:07 +0200

    Turned to TypeScript

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import remark from "remark";
 import html from "remark-html";
 
-const postsDirectory = path.join(process.cwd(), "posts")
+const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
     // Get file names under /posts
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
         // Combine the data with the id
         return {
             id,
-            ...matterResult.data
+            ...matterResult.data as { date: string; title: string; }
         }
     })
     // Sort posts by date
